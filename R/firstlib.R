dbe48173f779bb68ed3614c0d567e2f137afd80b
Joshua Justice <Joshua.Justice@gatech.edu>
2009-07-01 22:42:33 -0400

    I give up for the night. This is an improvement though.

--- R/firstlib.R
+++ R/firstlib.R
@@ -1,5 +1,7 @@
 # Set up the package
 .First.lib <- function(lib,pkg){
-	.jpackage("CensusMerge", lib.loc=lib, jars='*')
+	.jpackage(pkg,
+                  morePaths=file.path(lib, 'com/JoshuaJustice/CensusMerge/'),
+                  nativeLibrary=FALSE, lib.loc=lib)
 	cat("CensusMerge has been loaded.\n")
 }
