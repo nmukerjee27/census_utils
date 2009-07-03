cdc86931072c934b25e786f29e65be55d4cb2073
JoshJGT <Joshua.Justice@gatech.edu>
2009-07-03 16:14:22 -0400

    In reality there is no need for R here.

--- R/firstlib.R
+++
@@ -1,7 +0,0 @@
-# Set up the package
-.First.lib <- function(lib,pkg){
-	.jpackage(pkg,
-                  morePaths=file.path(lib, 'com/JoshuaJustice/CensusMerge/'),
-                  nativeLibrary=FALSE, lib.loc=lib)
-	cat("CensusMerge has been loaded.\n")
-}
